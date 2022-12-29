34c45a935cf3cc18efe67dce58ff69de1619527d
AmrAhmed119 <amreaglegolden@gmail.com>
2022-12-29 18:39:59 +0200

    Updating interface

--- src/app/Components/Home/inbox/inbox.component.ts
+++ src/app/Components/Home/inbox/inbox.component.ts
@@ -8,8 +8,11 @@ import { Email } from 'src/app/email';
 })
 export class InboxComponent implements OnInit {
 
-  emails : Email[] = [];
-  checkId = "0";
+  emails : Email[] = [];         //list of emails to be displayed
+  selectedEmails : Email[] = []  //list of selected emails upon checkbox
+  checkId = "0";                 //id of checkboxes besides emails
+  load = false;                  //loading screen
+  selecting = true;              //selecting mode
 
   constructor() { }
 
@@ -26,14 +29,94 @@ export class InboxComponent implements OnInit {
     this.emails.push(new Email("asd@gmail.com","Amr Ahmed","MohamedAnwar@gmail.com","25 Dec",msg,8))
     this.emails.push(new Email("asd@gmail.com","Amr Ahmed","MohamedAnwar@gmail.com","25 Dec",msg,9))
     this.emails.push(new Email("asd@gmail.com","Amr Ahmed","MohamedAnwar@gmail.com","25 Dec",msg,10))
-    this.emails.push(new Email("asd@gmail.com","Amr Ahmed","MohamedAnwar@gmail.com","25 Dec",msg,11))
-    this.emails.push(new Email("asd@gmail.com","Amr Ahmed","MohamedAnwar@gmail.com","25 Dec",msg,12))
+
   }
 
-  increment(event:any) {
-      if(event.target.checked) {
-        console.log(event.target.id)
+  toggleBg(event : any) {
+    let ob = event.target;
+    if(ob.checked === true) {
+      document.getElementsByName(event.target.id)[0].style.background = "#ececed";
+    }
+    else {
+      document.getElementsByName(event.target.id)[0].style.background = "#ffffff";
+    }
+  }
+
+  newestEmails() {
+    /*
+      REQUEST HERE
+      get emails
+
+    */
+
+  }
+
+  priorityEmails() {
+
+    /*
+      REQUEST HERE
+      get emails
+      
+    */
+
+  }
+
+  addEmail(event : any) {
+    if(event.target.checked === true) {
+        let id = event.target.id;
+        const found = this.emails.find((obj) => {
+          return obj.id == id;
+        });
+        this.selectedEmails.push(found!);
+    }
+  }
+
+  delete() {
+    let x = document.getElementById("bg");
+    this.load = true;
+    if(x != undefined) {
+      x.style.background = "#E5E7E9";
+    }
+    setTimeout(()=>{
+      for(let i=0;i<this.selectedEmails.length;i++) {
+        this.emails=this.emails.filter(x=> x.id!=this.selectedEmails[i].id)
       }
+      this.load = false;
+      if(x != undefined) {
+        x.style.background = "transparent";
+      }
+    }, 1000); 
+    
+  }
+
+  selectAll() {
+    
+    for(let i=0;i<this.emails.length;i++) {
+      let id = this.emails[i].id;
+      if(id != undefined) {
+        let checkbox = document.getElementById(id.toString()) as HTMLInputElement | null;
+        if(checkbox != null) {
+          checkbox.checked = false;
+        }
+      }
+    }
+
+    for(let i=0;i<this.emails.length;i++) {
+      let id = this.emails[i].id;
+      if(id != undefined) {
+        let checkbox = document.getElementById(id.toString()) as HTMLInputElement | null;
+        if(checkbox != null) {
+          if(checkbox.checked === true) {
+            checkbox.checked = false;
+            this.selectedEmails.pop();
+          }
+          else {
+            checkbox.checked = true;
+            this.selectedEmails.push(this.emails[i]);
+          }
+        }
+      }
+    }
   }
 
 }
